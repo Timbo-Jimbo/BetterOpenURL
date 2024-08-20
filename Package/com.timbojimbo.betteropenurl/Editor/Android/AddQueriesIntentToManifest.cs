75bd315ea1c219dadf32850218972f4e4271c346
Timbo Jimbo Mac Mini <timbo@timbojimbo.com>
2024-08-20 14:00:20 +1000

    made sure we dont pollute the manifest with copies of the same query tag

--- Package/com.timbojimbo.betteropenurl/Editor/Android/AddQueriesIntentToManifest.cs
+++ Package/com.timbojimbo.betteropenurl/Editor/Android/AddQueriesIntentToManifest.cs
@@ -1,12 +1,12 @@
 #if UNITY_ANDROID
 
 using UnityEditor.Android;
+using UnityEngine;
 
 namespace TimboJimbo.BetterOpenURL.Editor.Android
 {
     public class AddQueriesIntentToManifest : IPostGenerateGradleAndroidProject
     {
-
         public int callbackOrder => 0;
 
         public void OnPostGenerateGradleAndroidProject(string path)
@@ -27,18 +27,48 @@ namespace TimboJimbo.BetterOpenURL.Editor.Android
 
             var doc = new System.Xml.XmlDocument();
             doc.Load(manifestPath);
+            
+            var namespaceManager = new System.Xml.XmlNamespaceManager(doc.NameTable);
+            namespaceManager.AddNamespace("android", AndroidXmlNamespace);
+            
+            //try find existing better-open-url-query-root
+            {
+                var betterOpenUrlQueryRoot = doc.SelectSingleNode("/manifest/queries[@android:name='better-open-url-query-root']", namespaceManager);
+                
+                //remove it and the comment right before it
+                var parent = betterOpenUrlQueryRoot?.ParentNode;
+                
+                if(betterOpenUrlQueryRoot?.PreviousSibling?.NodeType == System.Xml.XmlNodeType.Comment)
+                    parent?.RemoveChild(betterOpenUrlQueryRoot.PreviousSibling);
+                parent?.RemoveChild(betterOpenUrlQueryRoot);
+            }
 
             var manifest = doc.SelectSingleNode("/manifest");
-        
-            var queries = doc.CreateElement("queries");
-            var intent = doc.CreateElement("intent");
-            var action = doc.CreateElement("action");
-            var actionAttribute = doc.CreateAttribute("android", "name", AndroidXmlNamespace);
-            actionAttribute.Value = "android.support.customtabs.action.CustomTabsService";
-            action.Attributes?.Append(actionAttribute);
-            intent.AppendChild(action);
-            queries.AppendChild(intent);
-            manifest.AppendChild(queries);
+            if(manifest == null) throw new System.Exception("Could not find manifest node in AndroidManifest.xml");
+            {
+                var comment = doc.CreateComment("BetterOpenURL: Required to detect if CustomTabsService is available on the device");
+                manifest.AppendChild(comment);
+
+                var queries = doc.CreateElement("queries");
+                {
+                    var nameAttribute = doc.CreateAttribute("android", "name", AndroidXmlNamespace);
+                    nameAttribute.Value = "better-open-url-query-root";
+                    queries.Attributes.Append(nameAttribute);
+                    
+                    var intent = doc.CreateElement("intent");
+                    {
+                        var action = doc.CreateElement("action");
+                        {
+                            var actionAttribute = doc.CreateAttribute("android", "name", AndroidXmlNamespace);
+                            actionAttribute.Value = "android.support.customtabs.action.CustomTabsService";
+                            action.Attributes.Append(actionAttribute);
+                        }
+                        intent.AppendChild(action);
+                    }
+                    queries.AppendChild(intent);
+                }
+                manifest.AppendChild(queries);
+            }
 
             doc.Save(manifestPath);
         }
